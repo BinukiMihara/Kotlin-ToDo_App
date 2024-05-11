db41e11ad138e460b02e9c16daa18c88f9c093ff
Binuki Mihara <133636115+BinukiMihara@users.noreply.github.com>
2024-05-11 23:00:27 +0530

    Create activity to add new notes

---
+++ app/src/main/java/com/example/todoapp/AddActivity.kt
@@ -0,0 +1,67 @@
+package com.example.todoapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputLayout
+import com.example.todoapp.db.DBOpenHelper
+
+class AddActivity : AppCompatActivity() {
+
+
+    private lateinit var etTitle: TextInputLayout
+    private lateinit var etDescription: TextInputLayout
+    private lateinit var fabSend: FloatingActionButton
+    private val dbOpenHelper = DBOpenHelper(this)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+
+        etTitle = findViewById(R.id.et_title)
+        etDescription = findViewById(R.id.et_description)
+        fabSend = findViewById(R.id.fab_send)
+
+        fabSend.setOnClickListener {
+            fabSendData()
+        }
+
+    }
+
+    private fun fabSendData() {
+
+        if (etTitle.editText?.text.toString().isEmpty()) {
+            etTitle.error = "Please enter your Title"
+            etTitle.requestFocus()
+            return
+        }
+
+        if (etDescription.editText?.text.toString().isEmpty()) {
+            etDescription.error = "Please enter your Description"
+            etDescription.requestFocus()
+            return
+        }
+
+        if (notEmpty()) {
+            dbOpenHelper.addToDo(
+                etTitle.editText?.text.toString(),
+                etDescription.editText?.text.toString()
+            )
+            Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show()
+            val intentToMainActivity = Intent(this, MainActivity::class.java)
+            startActivity(intentToMainActivity)
+            finish()
+        }
+
+    }
+
+
+    private fun notEmpty(): Boolean {
+        return (etTitle.editText?.text.toString().isNotEmpty()
+                && etDescription.editText?.text.toString().isNotEmpty())
+    }
+
+}
